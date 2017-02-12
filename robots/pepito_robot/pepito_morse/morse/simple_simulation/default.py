5b95c5206d10c79c7a69f9c369495b6317c73e38
Diego Escudero <dgerod@xyz-lab.org.es>
2017-02-12 19:07:21 +0100

    Updated simulation file and MoveIt! configuration for Pepito robot in MORSE.

---
+++ robots/pepito_robot/pepito_morse/morse/simple_simulation/default.py
@@ -0,0 +1,57 @@
+# =======================================================================================
+# Pepito with ROS
+# =======================================================================================
+
+from morse_helpers import morse_local_config as local_settings
+local_settings.configure_simulation(__file__)
+from morse_helpers.storage import FileStorage
+from morse_helpers.adapters import ROSRegister
+
+from morse.builder import Environment
+from morse.builder import Jido
+from morse.builder.actuators import KukaLWR
+from morse.builder.sensors import ArmaturePose, Odometry
+
+# ---------------------------------------------------------------------------------------
+
+def create_simulation():
+
+    # Create the robot
+    # ----------------------------------------------------------
+
+    robot = Jido()
+    robot.name = "pepito"
+
+    odometry = Odometry()
+    robot.append(odometry)
+
+    arm = KukaLWR()
+    arm.translate(x=0.176, y=0.178, z=.9)
+    arm.rotate(x=1.571, y=3.142)
+    arm_pose = ArmaturePose()
+    arm.append(arm_pose)
+    robot.append(arm)
+
+    # Set-up ROS connection
+    # ----------------------------------------------------------
+
+    robot.add_default_interface('ros')
+
+    ROSRegister.add_topic(arm_pose, "pepito/arm/joint_states", "ArmStatePublisher")
+    ROSRegister.add_controller(arm, "pepito/arm", "ArmCtrlByActions")
+    ROSRegister.add_topic(odometry, "/odom", child_frame_id="jido_base_footprint")
+
+    # Environment
+    # ----------------------------------------------------------
+
+    env = Environment(FileStorage.find("empty_apartment.blend"))
+    env.set_camera_location([2.0, -2.0, 4.0])
+
+    env.show_framerate(True)
+
+# ---------------------------------------------------------------------------------------
+
+create_simulation()
+
+# =======================================================================================
+
